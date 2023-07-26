2b717e687aedf9c9200f5a595046efb0d543ae60
Sam Blackburn <sam_blackburn@pobox.com>
2023-07-26 06:20:18 -0400

    Add version.py
    Useful for testing connecting to the database

--- Scripts/version.py
+++ Scripts/version.py
@@ -1,2 +1,11 @@
+import sys
+sys.path.append('C:/Program Files/Truxton/SDK')
 import truxton
 
+def main():
+  t = truxton.create()
+  print("Truxton Version: " + t.version)
+  return None
+
+if __name__ == "__main__":
+  main()
